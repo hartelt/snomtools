682c3cc7178fc08b61e30a0ddcb7a2fe53bec6bb
Michael Hartelt <hartelt@physik.uni-kl.de>
2015-06-09 17:58:42 +0200

    units.py: New import function for ureg. metals.py: uses units now.

--- src/snomtools/calcs/units.py
+++ src/snomtools/calcs/units.py
@@ -14,3 +14,21 @@ ureg = pint.UnitRegistry()
 
 # Custom prefixes we use frequently can be defined here:
 #ureg.define('myprefix- = 30 = my-')
+
+def to_ureg(input_,unit=None):
+	'''
+	This method is an import function to import alien quantities (of different unit registries) or numeric formats into the ureg.
+	:param input_: The input quantity or numeric format (e.g. float, int, numpy array)
+	:param unit: If a numeric format is used, this specifies the unit of it. Given as a valid unit string.
+	:return: The imported quantity.
+	'''
+
+	#Check if input is quantity:
+	if hasattr(input_,'_REGISTRY'):
+		#Check if input is already of our ureg. If so, nothing to do:
+		if input_._REGISTRY is ureg:
+			return input_
+		else: #Use inputs magnitude, but our corresponding ureg unit.
+			return input_.magnitude * ureg(str(input_.units))
+	else: #we are dealing with numerial data
+		return input_ * ureg(unit)
