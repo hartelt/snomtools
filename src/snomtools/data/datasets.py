d20ae22ccb247ca2908cc08dd7e44fbaee0788c7
Michael Hartelt <hartelt@physik.uni-kl.de>
2015-09-28 18:08:24 +0200

    Worked on load_textfile.

--- src/snomtools/data/datasets.py
+++ src/snomtools/data/datasets.py
@@ -8,6 +8,7 @@ import numpy
 import os
 import h5py
 import h5tools
+import string
 from termcolor import colored, cprint
 
 
@@ -232,6 +233,7 @@ class DataSet:
 			# print("axis len: "+str(len(self.axes[i])))
 			# print("data len: "+str(self.datafields[0].shape[i]))
 			assert (len(self.axes[i]) == self.datafields[0].shape[i]), "Axes lenghts don't fit to data dimensions."
+		return True
 
 	def saveh5(self, path):
 		path = os.path.abspath(path)
@@ -260,6 +262,51 @@ class DataSet:
 		self.plotconf = h5tools.load_dictionary(infile['plotconf'])
 		self.check_data_consistency()
 
+	def load_textfile(self, path, axis=0, comments='#', delimiter=None, **kwargs):
+		"""
+		Loads the contents of a textfile to the dataset instance. The text files are two-dimensional arrays of lines
+		and n columns, so it can hold up to one axis and (n-1) or n DataFields.
+		Uses numpy.loadtxt() under the hood!
+		Defaults fit for gnuplot friendly files. Tries to cast the heading comment line(s) as labels and units for
+		the data fields.
+		:param path: The (relative or absolute) path of the text file to read.
+		:param axis: Integer that specifies the column of the axis in the data file OR a whole axis instance of the
+		correct shape OR None for unchanged axes of the DataSet. Default is 0 for the first column of the text file.
+		:param comments: the character used to indicate the start of a comment line
+		:param delimiter: character used to separate values. By default, this is any whitespace.
+		:param kwargs: Keyword arguments as used for the numpy.loadtxt() method
+		:return: Nothing
+		"""
+		# Normalize path:
+		path = os.path.abspath(path)
+		# Load data from text file:
+		datacontent = numpy.loadtxt(path, comments=comments, delimiter=delimiter, **kwargs)
+		# All columns contain data by default:
+		datacolumns = range(datacontent.shape[1])
+
+		#TODO: Handle heading comment lines.
+
+		# If we should handle axis:
+		if not (axis is None):
+			if type(axis)==int: # Column number was given.
+				datacolumns.remove(axis) # Column contains axis and not data.
+				self.axes=[Axis(datacontent[:,axis])] # Initialize axis
+			elif type(axis)==Axis: # Complete axis was given.
+				self.axes=[axis]
+			elif type(axis)==DataArray: # DataArray was given for axis.
+				self.axes=[Axis.from_dataarray(axis)]
+			else: # We don't know what was given... Let's try to cast it as an axis.
+				try:
+					self.axes=[Axis(axis)]
+				except Exception as e:
+					print colored("ERROR! Axis initialization in load_textfile failed.","red")
+					raise e
+		self.datafields = [] # Reseet datafields
+		for i in datacolumns: # Initialize new datafields
+			self.datafields.append(DataArray(datacontent[:,i]))
+
+		return self.check_data_consistency()
+
 	def __del__(self):
 		pass
 
@@ -281,6 +328,8 @@ if True:  # just for testing
 	testdataset.saveh5('test.hdf5')
 	print("Load...")
 	newdataset = DataSet.from_h5file('test.hdf5')
+	newdataset.load_textfile('test.txt', dtype='float', comments='#')
+	print(newdataset)
 	print(newdataset.axes)
-	print(newdataset.plotconf)
+	print(newdataset.datafields)
 	cprint("OK", 'green')
