845750f725dc4d1c20ae0bc973a805f30ff09f6b
Michael Hartelt <hartelt@physik.uni-kl.de>
2016-06-08 17:02:29 +0200

    dataset plotting file created. Inital docstring and imports.

---
+++ src/snomtools/plots/datasets.py
@@ -0,0 +1,8 @@
+"""
+This file provides scripts for common plotting applications working on DataSets as in snomtools.data.datasets.
+They use matplotlib.
+"""
+
+import snomtools.data.datasets
+import snomtools.plots.setupmatplotlib as plt
+import numpy
