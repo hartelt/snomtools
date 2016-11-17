a28fe5625505fd294e259faeff0a7e1142637f43
Michael Hartelt <hartelt@physik.uni-kl.de>
2016-11-17 19:20:18 +0100

    Tobi Doc more example

--- src/snomtools/__init__.py
+++ src/snomtools/__init__.py
@@ -4,6 +4,8 @@
 This Package contains various tools written in Python and oriented to analysis and presentation of data.
 
 Tobi doc example as follows:
+Base definitions
+++++++++++++++++
 Furthermore the unit bias may interfere the number conversion.
 
     >>> t = Time(name="t", value="10 ms")
