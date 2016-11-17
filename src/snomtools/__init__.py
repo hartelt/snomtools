502937ab9e4c2cc106879ddf32901d91fc3c5113
Michael Hartelt <hartelt@physik.uni-kl.de>
2016-11-17 19:15:52 +0100

    Tobi Doc Debugging

--- src/snomtools/__init__.py
+++ src/snomtools/__init__.py
@@ -1,4 +1,13 @@
-__author__ = 'Michael Hartelt, Cristian Gonzalez'
-'''
+"""
 This Package contains various tools written in Python and oriented to analysis and presentation of data.
-'''
+
+Tobi doc example as follows:
+Furthermore the unit bias may interfere the number conversion.
+
+    >>> t = Time(name="t", value="10 ms")
+    >>> t.number
+    0.01
+
+.. autoclass:: lapp.variables.variable.Variable
+"""
+__author__ = 'Michael Hartelt, Cristian Gonzalez'
