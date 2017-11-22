20672a2266c5ab52ce6deb0ae19a59da1a769fd0
Michael Hartelt <hartelt@physik.uni-kl.de>
2017-11-22 23:34:04 +0100

    plots.evaluations: Powerlaw: Flexible option for logscale or not.

--- src/snomtools/plots/evaluations.py
+++ src/snomtools/plots/evaluations.py
@@ -6,7 +6,7 @@ They use matplotlib.
 import numpy
 
 
-def plot_powerlaw(powerlaw, plot_dest, resolution=1000, legend_loc='lower right'):
+def plot_powerlaw(powerlaw, plot_dest, resolution=1000, legend_loc='lower right', log=True):
 	"""
 	Plots a powerlaw, meaning the intensity values and corresponding linear fit in the double logarithmic plot.
 
@@ -30,8 +30,11 @@ def plot_powerlaw(powerlaw, plot_dest, resolution=1000, legend_loc='lower right'
 				   powerlaw.data.get_datafield(0).get_data(),
 				   'o', label="Counts")
 	plot_dest.plot(xforfunc, powerlaw.y(xforfunc), '-', label="Fit with " + str(powerlaw.poly))
-	plot_dest.set_xscale("log")
-	plot_dest.set_yscale("log")
+	if log:
+		plot_dest.set_xscale("log")
+		plot_dest.set_yscale("log")
+	plot_dest.set_xlabel(powerlaw.data.get_axis(0).plotlabel)
+	plot_dest.set_ylabel(powerlaw.data.get_datafield(0).plotlabel)
 	if legend_loc:
 		if not (legend_loc in ["None", "none"]):
 			plot_dest.legend(loc=legend_loc)
