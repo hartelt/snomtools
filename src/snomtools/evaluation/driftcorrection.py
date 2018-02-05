dd9b8919fb9f6b81f722b70f6e69fc58ca826142
bfrisch <b.f.3000@gmx.net>
2018-02-05 15:22:07 +0100

    Documentation
    Added documentation for all functions

--- src/snomtools/evaluation/driftcorrection.py
+++ src/snomtools/evaluation/driftcorrection.py
@@ -1,6 +1,7 @@
 __author__ = 'frisch'
 ''' 
-This file provides driftkorrection for any array stack. 
+This file provides driftkorrection for array stacks. It generates the drift vectors via 
+Crosscorrelation-Methods provided by the OpenCV librariy 
 '''
 
 import cv2 as cv
@@ -11,8 +12,18 @@ import snomtools.data.datasets
 class Drift:
 	def __init__(self, data=None, template=None, stackAxisID=None, yAxisID=None, xAxisID=None,
 				 subpixel=True, method='cv.TM_CCOEFF_NORMED'):
+		'''
+		Calculates the correlation of a given 2D template with all slices in a n-D dataset which gets projected onto the three axes stackAxis, yAxis, xAxis.
+		Differend methods and subpixel accuracy are available.
+		:param data: n-D dataset
+		:param template: 2D template array
+		:param stackAxisID: Axis, along which the template_matching is calculated
+		:param yAxisID: ID of the first axis of the image, i.e. y
+		:param xAxisID: ID of the second axis of the image, i.e. x
+		:param subpixel: Generate subpixel accurate drift vectors
+		:param method: Method to calculate the Correlation between template and data. Possible methods: 'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED' (default), 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
+		'''
 
-		# Methods: 'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
 
 		# read axis
 		if data:
@@ -54,6 +65,15 @@ class Drift:
 
 	@classmethod
 	def template_matching_stack(cls, data, template, stackAxisID, method='cv.TM_CCOEFF_NORMED', subpixel=True):
+		'''
+		Passes the data of a 3D array along the stackAxis in form of 2D data to the template_matching function
+		:param data: 3D dataset
+		:param template: 2D template which is used to calculate the correlation
+		:param stackAxisID: Axis along which the 2D data is extracted and passed for template_matching
+		:param method: Method to calculate the Correlation between template and data. Possible methods: 'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED' (default), 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
+		:param subpixel: Generate subpixel accurate drift vectors
+		:return: List of tuples containing the coordinates of best correlation
+		'''
 		driftlist = []
 		for i in range(data.shape[stackAxisID]):
 			slicebase = [np.s_[:], np.s_[:]]
@@ -64,7 +84,16 @@ class Drift:
 
 	@staticmethod
 	def template_matching(data_to_match, template, method='cv.TM_CCOEFF_NORMED', subpixel=True):
-		# Methods: 'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
+		'''
+		Uses the openCV matchTemplate function to calculate a correlation between a 2D template array and the 2D data array
+		The coordinates of the maximum (or minimum for SQDIFF methods) of the calculated correlation are returned.
+		:param data_to_match: 2D dataset
+		:param template: 2D template which is used to calculate the correlation
+		:param method: Method to calculate the Correlation between template and data. Possible methods: 'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED' (default), 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
+		:param subpixel: Generate subpixel accurate drift vectors
+		:return: Returns the coordinate (y,x) of the correlation maximum referenced to the top left corner of the template.
+		'''
+		#
 
 		method = eval(method)
 
@@ -74,13 +103,13 @@ class Drift:
 		res = cv.matchTemplate(data_to_match, template, method)
 
 		if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-			min_loc = np.unravel_index(res.argmin(),res.shape)
+			min_loc = np.unravel_index(res.argmin(), res.shape)
 			if subpixel:
 				top_left = Drift.subpixel_peak(min_loc, res)
 			else:
 				top_left = min_loc
 		else:
-			max_loc = np.unravel_index(res.argmax(),res.shape)
+			max_loc = np.unravel_index(res.argmax(), res.shape)
 			if subpixel:
 				top_left = Drift.subpixel_peak(max_loc, res)
 			else:
@@ -90,9 +119,16 @@ class Drift:
 
 	@staticmethod
 	def subpixel_peak(max_var, results):
+		'''
+		Extrapolates the position of a maximum in a 2D array by logarithmical evaluation of it's surrounding values.
+		I.e. for x: max_new=max + ( log(f(x-1)) - log(f(x+1)) ) / ( 2*log(f(x-1)) + 2*log(f(x+1)) - 4*log(f(x)) )
+
+		:param max_var: tuple with position of the maximum
+		:param results: array with the values in which the subpixel maximum is to be found. Has to contain at least the neighbouring elements of max_var.
+		:return: tuple of subpixel accurate maximum position
+		'''
 		y = max_var[0]
 		x = max_var[1]
-		coord = []
 
 		y_sub = y \
 				+ (np.log(results[y - 1, x]) - np.log(results[y + 1, x])) \
@@ -106,12 +142,27 @@ class Drift:
 
 	@staticmethod
 	def extract_3Ddata(data, stackAxisID, yAxisID, xAxisID):
+		'''
+		Projects the data of a n-D dataset onto the 3D system of stackAxis, yAxis, xAxis.
+		:param data:
+		:param stackAxisID: Defines the stack axis that is not projected and can be use for template_matching
+		:param yAxisID: ID of the first axis of the image, i.e. y
+		:param xAxisID: ID of the second axis of the image, i.e. x
+		:return: Dataset projected on the defined axes while keeping the stackAxis
+		'''
 		assert isinstance(data, snomtools.data.datasets.DataSet), \
 			"ERROR: No dataset or ROI instance given to extract_3Ddata."
 		return data.project_nd(stackAxisID, yAxisID, xAxisID)
 
 	@staticmethod
 	def extract_templatedata(data, yAxisID, xAxisID):
+		'''
+		Transforms the data of a template for further processing in the template_matching method.
+		:param data: Raw dataset as quantity with axisID's
+		:param yAxisID: ID of the first axis, i.e. y
+		:param xAxisID: ID of the second axis, i.e. x
+		:return: Data of the whole template projected onto the two defined axes.
+		'''
 		assert isinstance(data, snomtools.data.datasets.DataSet) or isinstance(data, snomtools.data.datasets.ROI), \
 			"ERROR: No dataset or ROI instance given to extract_templatedata."
 
@@ -122,6 +173,13 @@ class Drift:
 
 	@staticmethod
 	def guess_templatedata(data, yAxisID, xAxisID):
+		'''
+		Generates a default template for the template_matching method. The template is generated in the center between 2/5 and 3/5 of the image.
+		:param data: Any dataset as quantity with axisID's
+		:param yAxisID: ID of the first axis, i.e. y
+		:param xAxisID: ID of the second axis, i.e. x
+		:return: Projected ROI of the 2/5 to 3/5 field.
+		'''
 		assert isinstance(data, snomtools.data.datasets.DataSet) or isinstance(data, snomtools.data.datasets.ROI), \
 			"ERROR: No dataset or ROI instance given to guess_template."
 
@@ -129,7 +187,7 @@ class Drift:
 		xAxisID = data.get_axis_index(xAxisID)
 		fieldshape = (data.shape[yAxisID], data.shape[xAxisID])
 		yl, yr, xl, xr = fieldshape[0] * 2 / 5, fieldshape[0] * 3 / 5, fieldshape[1] * 2 / 5, fieldshape[1] * 3 / 5
-		limitlist = {yAxisID: (yl, yr),xAxisID: (xl, xr)}
+		limitlist = {yAxisID: (yl, yr), xAxisID: (xl, xr)}
 		roi = snomtools.data.datasets.ROI(data, limitlist, by_index=True)
 		return roi.project_nd(yAxisID, xAxisID).get_datafield(0)
 
