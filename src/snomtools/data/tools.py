0d743f47fbad9c370c85dc298f15012ea96e8884
Michael Hartelt <hartelt@physik.uni-kl.de>
2018-02-14 22:14:10 +0100

    data.tools: More slicing tools.

--- src/snomtools/data/tools.py
+++ src/snomtools/data/tools.py
@@ -31,7 +31,7 @@ def assure_1D(data):
 		return flatarray
 
 
-def full_slice(slice_, len_):
+def full_slice(slice_, len_=None):
 	"""
 	Generate a full slice tuple from a general slice tuple, which can have entries for only some of the first
 	dimensions.
@@ -39,7 +39,8 @@ def full_slice(slice_, len_):
 	:param slice_: The incomplete slice, as generated with numpy.s_[something].
 	:type slice_: tuple **or** slice **or** int
 
-	:param int len_: The length of the full slice tuple (number of dimensions).
+	:param int len_: The length of the full slice tuple (number of dimensions). If not given, input is only returned as
+		tuple.
 
 	:return: The complete slice.
 	:rtype: tuple
@@ -48,8 +49,10 @@ def full_slice(slice_, len_):
 		slice_ = tuple(np.s_[slice_])
 	except TypeError:
 		slice_ = (np.s_[slice_],)
-	missing = tuple([np.s_[:] for i in range(len_ - len(slice_))])
-	return slice_ + missing
+	if len_ is not None:
+		missing = tuple([np.s_[:] for i in range(len_ - len(slice_))])
+		slice_ = slice_ + missing
+	return slice_
 
 
 def sliced_shape(slice_, shape_):
@@ -75,6 +78,82 @@ def sliced_shape(slice_, shape_):
 		except TypeError:
 			pass
 	return tuple(sizes)
+
+
+def slice_expansion(inslice, expansion, dimensions=None):
+	"""
+	Extends a given slice in both directions by given values.
+
+	:param inslice: The slice to be extended, as produced with numpy.s_[something]
+	:type inslice: tuple **or** slice **or** int
+
+	:param expansion: The value or values to expand the slice by. If an int is given, expand all dimensions by that
+		value. If a tuple is given, it should contain one int for each dimension, by which the dimension is then
+		expanded.
+	:type expansion: int **or** tuple(int)
+
+	:param dimensions: (optional) A shape of the data to be addressed by the extended slice. If this is given, a full
+		slice with elements for every axis is returned.
+	:type dimensions: int
+
+	:return: The expanded slice.
+	:rtype: tuple( int / slice )
+	"""
+	inslice = full_slice(inslice, dimensions)
+	try:
+		expansion = tuple(expansion)
+	except TypeError:
+		expansion = tuple([int(expansion) for i in range(len(inslice))])
+	assert len(expansion) == len(inslice), "Expansion tuple with invalid length given."
+	newslice = []
+	for slice_, exp in zip(inslice, expansion):
+		if isinstance(slice_, slice):
+			start, stop = slice_.start, slice_.stop
+			if start is not None:
+				start -= exp
+			if stop is not None:
+				stop += exp
+			newslice.append(np.s_[start: stop: slice_.step])
+		else:  # Scalar, int-like
+			newslice.append(np.s_[slice_ - exp: slice_ + exp: None])
+	return tuple(newslice)
+
+
+def slice_overhang(slice_, shape_):
+	"""
+	Calculate the overhang of an expanded slice over the edge of the data shape.
+
+	.. warning::
+		Slices with step statements are not supported yet.
+
+	:param slice_: The extended full tuple of slice objects (and ints).
+	:type slice_: tuple
+
+	:param tuple shape_: The shape tuple of the addressed array-like.
+
+	:return: A tuple of len(shape_) 2-tuples of ints. containing the overhang in the form (negative,positive).
+	:rtype: tuple
+	"""
+	assert len(slice_) == len(shape_), "Invalid argument lengths."
+	overhangs = []
+	for sl, sh in zip(slice_, shape_):
+		if isinstance(sl, slice):
+			start, stop, step = sl.start, sl.stop, sl.step
+			if step is None:
+				if start is not None:
+					left = - min(start, 0)
+				else:
+					left = 0
+				if stop is not None:
+					right = max(stop - sh, 0)
+				else:
+					right = 0
+			else:
+				raise NotImplementedError()
+			overhangs.append((left, right))
+		else:  # scalar, int-like
+			overhangs.append((0, 0))
+	return tuple(overhangs)
 
 
 def iterfy(iterable):
