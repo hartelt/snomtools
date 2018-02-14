54cae43d706d1afbec63d6cbfd86adaae54e600c
Michael Hartelt <hartelt@physik.uni-kl.de>
2018-02-14 18:05:55 +0100

    data.tools: Tools for slicing and iteration, to be used in driftcorrection.

--- src/snomtools/data/tools.py
+++ src/snomtools/data/tools.py
@@ -7,6 +7,8 @@ data.datasets.py
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from six import string_types
+import numpy as np
 
 __author__ = 'hartelt'
 
@@ -27,3 +29,63 @@ def assure_1D(data):
 		raise ArithmeticError("Non-1D convertable data array in Axis.")
 	else:
 		return flatarray
+
+
+def full_slice(slice_, len_):
+	"""
+	Generate a full slice tuple from a general slice tuple, which can have entries for only some of the first
+	dimensions.
+
+	:param slice_: The incomplete slice, as generated with numpy.s_[something].
+	:type slice_: tuple **or** slice **or** int
+
+	:param int len_: The length of the full slice tuple (number of dimensions).
+
+	:return: The complete slice.
+	:rtype: tuple
+	"""
+	try:
+		slice_ = tuple(np.s_[slice_])
+	except TypeError:
+		slice_ = (np.s_[slice_],)
+	missing = tuple([np.s_[:] for i in range(len_ - len(slice_))])
+	return slice_ + missing
+
+
+def sliced_shape(slice_, shape_):
+	"""
+	Calculate the shape one would get by slicing an array of shape :code:`shape_` with a slice :code:`slice_`.
+
+	.. note:: This is propably very inefficient, because an 1D-Array is initialized and sliced for each dimension.
+
+	:param slice_: A slice, as generated with numpy.s_[something].
+
+	:param tuple shape_: The shape of the hypothetical array to be sliced.
+
+	:return: The resulting shape.
+	:rtype: tuple
+	"""
+	full_slice_ = full_slice(slice_, len(shape_))
+	sizes = []
+	for i in range(len(shape_)):
+		arr = np.empty(shape_[i])
+		subarr = arr[full_slice_[i]]
+		try:
+			sizes.append(len(subarr))
+		except TypeError:
+			pass
+	return tuple(sizes)
+
+
+def iterfy(iterable):
+	"""
+	Makes sure a given object can be iterated over: If an iterable is given, it just returns it. If a non-iterable
+		e.g. a single value is given, it returns a list containing the input as a single element.
+	"""
+	if isinstance(iterable, string_types):
+		iterable = [iterable]
+	try:
+		iter(iterable)
+	except TypeError:
+		iterable = [iterable]
+	return iterable
