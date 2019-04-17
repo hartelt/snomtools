4da89c5d98836e1e85a7165e111945664d299559
Michael Hartelt <hartelt@physik.uni-kl.de>
2019-04-17 16:53:17 +0200

    data.transformation.binning: New file stub for binning.

---
+++ src/snomtools/data/transformation/binning.py
@@ -0,0 +1,26 @@
+"""
+This script holds transformation functions for datasets, that do a binning of data to reduce data dimensions.
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import snomtools.data.datasets as ds
+import snomtools.calcs.units as u
+
+raise NotImplementedError()
+
+# For verbose mode with progress printouts:
+if '-v' in sys.argv:
+	verbose = True
+	import time
+else:
+	verbose = False
+
+if __name__ == '__main__':  # Just for testing:
+	print("Testing...")
+
+	print("done.")
