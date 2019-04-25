9e6beaa4c74f5ffd91387055b3778d3dfb26d988
Michael Hartelt <hartelt@physik.uni-kl.de>
2019-04-25 19:03:17 +0200

    First working version of bin_data.

--- src/snomtools/data/transformation/binning.py
+++ src/snomtools/data/transformation/binning.py
@@ -11,9 +11,6 @@ import sys
 import numpy as np
 
 import snomtools.data.datasets as ds
-import snomtools.calcs.units as u
-
-#raise NotImplementedError()
 
 # For verbose mode with progress printouts:
 if '-v' in sys.argv:
@@ -25,7 +22,7 @@ else:
 
 class Binning(object):
 
-	def __init__(self, data = None, binAxisID = None, binFactor = None):
+	def __init__(self, data=None, binAxisID=None, binFactor=None):
 
 		self.data = data
 
@@ -34,9 +31,7 @@ class Binning(object):
 		else:
 			self.binAxisID = data.get_axis_index(binAxisID)
 
-
 		self.binFactor = binFactor
-
 
 	def reshape_axis(self):
 		"""
@@ -44,39 +39,57 @@ class Binning(object):
 		:return:
 		"""
 		oldaxis = self.data.get_axis(self.binAxisID)
-		ticks = np.zeros(np.int8(oldaxis.shape[0]/self.binFactor))
-		newaxis = ds.Axis(data=ticks, unit=oldaxis.get_unit(),label=oldaxis.get_label() + ' binned', plotlabel=oldaxis.get_plotlabel()) #Make more elegant
-		for i in range (np.int8(oldaxis.shape[0]/self.binFactor)):
-			newaxis[i] = np.mean(oldaxis.get_data()[self.binFactor*i:self.binFactor*(i+1)])
+		ticks = np.zeros(np.int8(oldaxis.shape[0] / self.binFactor))
+		newaxis = ds.Axis(data=ticks, unit=oldaxis.get_unit(), label=oldaxis.get_label() + ' binned',
+						  plotlabel=oldaxis.get_plotlabel())  # Make more elegant
+		for i in range(np.int8(oldaxis.shape[0] / self.binFactor)):
+			newaxis[i] = np.mean(oldaxis.get_data()[self.binFactor * i:self.binFactor * (i + 1)])
 		return newaxis
 
-
 	def bin_data(self, h5target=None):
-		axList = range(self.data.axes.__len__())
-		# ToDo: move binAxis to last position
-		axList.remove(self.binAxisID)
-		#ToDo: generate new data array of right shape
-		newdata = self.data
+		newshape = list(self.data.shape)
+		newshape[self.binAxisID] = np.int8(newshape[self.binAxisID] / self.binFactor)
+		newdata = ds.Data_Handler_H5(shape=newshape, unit=self.data.get_datafield(0).get_unit())
 
-		for i in range(np.int8(self.data.get_axis(self.binAxisID).shape[0] / self.binFactor)):
-			#ToDo: adress correct datafield
-			newdata[i] = [[row[axis] for row in matrix] for axis in axList]
-			itertools.product
+		if verbose:
+			import time
+			print("Start:")
+			start_time = time.time()
+			print(time.ctime())
+		for chunkslice in newdata.iterchunkslices():
+			olddataregion = list(chunkslice)
+			olddataregion[self.binAxisID] = slice(chunkslice[self.binAxisID].start * self.binFactor,
+												  (chunkslice[self.binAxisID].start + newdata.chunks[self.binAxisID])
+												  * self.binFactor,
+												  None)
+			olddataregion = tuple(olddataregion)
+			olddata = self.data.get_datafield(0).data[olddataregion]
 
+			for i in range(self.binFactor):
+				poslist = [np.s_[:] for j in range(self.data.dimensions)]
+				poslist[self.binAxisID] = np.s_[i::self.binFactor]
+				newdata[chunkslice] += olddata[tuple(poslist)]
+		newdata = ds.DataArray(newdata,
+							   label="binned_" + self.data.get_datafield(0).label,
+							   plotlabel=self.data.get_datafield(0).plotlabel,
+							   h5target=h5target)
+		if verbose:
+			print("End:")
+			print(time.ctime())
+			print("{0:.2f} seconds".format(time.time() - start_time))
 		return newdata
 
 
 if __name__ == '__main__':  # Just for testing:
 	print("Testing...")
-	path = 'E:\\NFC15\\20171207 ZnO+aSiH\\01 DLD PSI -3 to 150 fs step size 400as\\Maximamap\\Driftcorrected\\summed_runs'
-	data_dir = 'E:\\NFC15\\20171207 ZnO+aSiH\\01 DLD PSI -3 to 150 fs step size 400as\\Maximamap\\Driftcorrected\\summed_runs\\projected.hdf5'
-	data = ds.DataSet.from_h5file(data_dir, h5target=path + '\\uselesscache.hdf5')
-	binSet = Binning(data=data, binAxisID= 'energy', binFactor= 3 )
+	# path = 'E:\\NFC15\\20171207 ZnO+aSiH\\01 DLD PSI -3 to 150 fs step size 400as\\Maximamap\\Driftcorrected\\summed_runs'
+	# data_dir = 'E:\\NFC15\\20171207 ZnO+aSiH\\01 DLD PSI -3 to 150 fs step size 400as\\Maximamap\\Driftcorrected\\summed_runs\\projected.hdf5'
+	# data = ds.DataSet.from_h5file(data_dir, h5target=path + '\\uselesscache.hdf5')
+
+	data = ds.DataSet.from_h5file("terra-tr-psi-dld.hdf5")
+
+	binSet = Binning(data=data, binAxisID='channel', binFactor=3)
 	newaxis = binSet.reshape_axis()
 	newdata = binSet.bin_data()
-
-	data.add_axis(newaxis)
-	data.add_datafield(newdata)
-	print(data.shape)
 
 	print("done.")
