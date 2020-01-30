2f15c1abfd2cc110635a22a1d8c31348203ca06f
Lukas Hellbrück <hellbrue@rhrk.uni-kl.de>
2020-01-30 15:08:38 +0100

    Added cuda submodule in evaluation with init.

---
+++ src/snomtools/evaluation/cuda/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+"""
+This module provides data processing scripts for the evaluation of measured data.
+It uses the modules data, calcs and plots.
+There shall be python scripts for different types of data, E.g. SNOM and PEEM images and image series of different
+types (time-resolved, energy-resolved, spectral-resolved, power-law, wavelength scans).
+
+"""
+
+__author__ = "Lukas Hellbrück"
