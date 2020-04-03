5b3feff8268ba05c20f6db22c599ef6339c949c7
Michael Hartelt <hartelt@physik.uni-kl.de>
2020-04-03 13:12:14 +0200

    evaluation.cuda.cuda_test: Moved testing to main clause to avoid execution on import.

--- src/snomtools/evaluation/cuda/cuda_test.py
+++ src/snomtools/evaluation/cuda/cuda_test.py
@@ -2,31 +2,38 @@
 This is the most minimal example to test if pycuda actually runs on your machine and calculates data.
 """
 
-import pycuda.autoinit
-import pycuda.driver as drv
-import numpy
 
-from pycuda.compiler import SourceModule
-mod = SourceModule(
-"""
-__global__ void multiply_them(float *dest, float *a, float *b)
-{
-  const int i = threadIdx.x;
-  dest[i] = a[i] * b[i];
-}
-"""
-)
+def test():
+    import pycuda.autoinit
+    import pycuda.driver as drv
+    import numpy
+    from pycuda.compiler import SourceModule
 
-multiply_them = mod.get_function("multiply_them")
+    mod = SourceModule(
+        """
+    __global__ void multiply_them(float *dest, float *a, float *b)
+    {
+      const int i = threadIdx.x;
+      dest[i] = a[i] * b[i];
+    }
+    """
+    )
 
-a = numpy.random.randn(400).astype(numpy.float32)
-b = numpy.random.randn(400).astype(numpy.float32)
+    multiply_them = mod.get_function("multiply_them")
 
-dest = numpy.zeros_like(a)
-multiply_them(
-        drv.Out(dest), drv.In(a), drv.In(b),
-        block=(400,1,1), grid=(1,1))
+    a = numpy.random.randn(400).astype(numpy.float32)
+    b = numpy.random.randn(400).astype(numpy.float32)
 
-print (dest-a*b)
-if numpy.allclose(dest, a*b):
+    dest = numpy.zeros_like(a)
+    multiply_them(drv.Out(dest), drv.In(a), drv.In(b), block=(400, 1, 1), grid=(1, 1))
+
+    print(dest - a * b)
+    if numpy.allclose(dest, a * b):
         print("SUCCESS!!!")
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    test()
