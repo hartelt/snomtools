95d7b8f95f732528bd8c6331bc402166c0b6befe
Martin Anstett <anstett@rhrk.uni-kl.de>
2020-04-21 16:57:04 +0200

    first version of normalize_along_axisnormalize_along_axis added

--- src/snomtools/data/transformation/normalize.py
+++ src/snomtools/data/transformation/normalize.py
@@ -72,3 +72,49 @@ def normalize_by_reference(data, refdata, data_id=0, refdata_id=0, exclude_axes=
 	data_normalized[~ np.isfinite(data_normalized)] = 0  # set inf, and NaN to 0
 	data.add_datafield(data_normalized, label=newlabel, plotlabel=new_plotlabel)
 	return data
+
+
+def normalize_along_axis(data, axes, data_id=0,
+							   mode="division",
+							   newlabel='normalizeddata',
+							   new_plotlabel="Normalized Data"):
+	"""
+	Normalizes a dataset along axes.
+	The normalized data is written into a new DataArray in the given DataSet.
+
+	:param data: The DataSet instance of the data to normalize.
+
+	:param axes: Nomalization of the data along these axes.
+
+	:param data_id: A valid identifier of the DataArray in the DataSet instance to apply normalization to. Per
+		default, the first DataArray is taken.
+
+
+	:param mode: The mode how the calculation between the data and reference should be done, Valid options:
+		"division", "divide", "div": Divide every pixel of the data by the corresponding pixel of the reference.
+		"subtraction", "subtract", "sub": Subtract every pixel of the data by the corresponding pixel of the reference.
+
+	:param newlabel: The label to set for the created DataArray.
+
+	:param new_plotlabel: The plotlabel to set for the created DataArray.
+
+	:return: The modified dataset.
+	"""
+
+	assert isinstance(data, ds.DataSet), "ERROR: No DataSet given or imported."
+
+	countsdata = data.get_datafield(data_id).get_data()
+	axindexlist = []
+	for axis in axes:
+		axindexlist.append(data.get_axis_index(axis))
+	axindexes = tuple(axindexlist)
+	normalized_max = countsdata / countsdata.max(axis=axindexes, keepdims=True)
+	normalized_max[~ np.isfinite(normalized_max)] = 0  # set inf, and NaN to 0
+	data.add_datafield(normalized_max, label=newlabel+'max', plotlabel=new_plotlabel+'max')
+
+	normalized_mean = countsdata / countsdata.mean(axis=axindexes, keepdims=True)
+	normalized_mean[~ np.isfinite(normalized_mean)] = 0  # set inf, and NaN to 0
+	data.add_datafield(normalized_mean, label=newlabel+'mean', plotlabel=new_plotlabel+'mean')
+
+	return data
+
