463a2962151195253612fcae68bea241ee411d63
Martin Anstett <anstett@rhrk.uni-kl.de>
2020-06-22 18:09:56 +0200

    Function normalize_sensitivity added to use the sensitivity of a detector as refdata

--- src/snomtools/data/transformation/normalize.py
+++ src/snomtools/data/transformation/normalize.py
@@ -127,7 +127,7 @@ def normalize_along_axis(data, axes, data_id=0,
 	elif ref == 'absmin':
 		refquantity = normalized_data.absmin(axis=axindexes, keepdims=True)
 	else:
-		raise ValueError("Unrecognized reference for normalize_along_axis.")
+		raise ValueError("Unrecognized axis for normalize_along_axis.")
 
 	if mode in ["division", "divide", "div"]:
 		normalized_data = normalized_data / refquantity
@@ -143,4 +143,67 @@ def normalize_along_axis(data, axes, data_id=0,
 	return data
 
 
-# TO DO: def normalize_sensitivity()
+def normalize_sensitivity(data, sensitivity, data_id=0, sensitivity_id=0, include_axes=None,
+						   mode="division",
+						   newlabel='normalizeddata',
+						   new_plotlabel="Normalized Data"):
+	"""
+	Normalizes a dataset by the sensitivity which is given by the data of another set.
+	The normalized data is written into a new DataArray in the given DataSet.
+	Data and sensitivity dimensions must be compatible.
+
+	:param data: The DataSet instance of the data to normalize.
+
+	:param sensitivity: The DataSet instance of the sensitivity (reference data).
+
+	:param data_id: A valid identifier of the DataArray in the DataSet instance to apply normalization to. Per
+		default, the first DataArray is taken.
+
+	:param sensitivity_id: A valid identifier of the DataArray in the reference DataSet instance to take as reference. Per
+		default, the first DataArray is taken.
+
+	:param include_axes: A list of valid axes identifiers of the reference data to include during normalization.
+
+	:param mode: The mode how the calculation between the data and reference should be done, Valid options:
+		"division", "divide", "div": Divide every pixel of the data by the corresponding pixel of the reference.
+		"subtraction", "subtract", "sub": Subtract every pixel of the data by the corresponding pixel of the reference.
+
+	:param newlabel: The label to set for the created DataArray.
+
+	:param new_plotlabel: The plotlabel to set for the created DataArray.
+
+	:return: The modified dataset.
+	"""
+	assert isinstance(data, ds.DataSet), "ERROR: No DataSet given or imported."
+	assert isinstance(sensitivity, ds.DataSet), "ERROR: No DataSet given or imported."
+
+	# Assemble tuple of axis indices to project onto:
+	if include_axes:
+		#get the exclude axes
+		exclude_axes = sensitivity.axlabels
+		for include_axis in include_axes:
+			exclude_axes.remove(include_axis)
+		sumlist = []
+		for exclude_axis in exclude_axes:
+			sumlist.remove(sensitivity.get_axis_index(exclude_axis))
+		sumtup = tuple(sumlist)
+		refquantity = sensitivity.get_datafield(sensitivity_id).sum(sumtup)
+	else:
+		refquantity = sensitivity.get_datafield(sensitivity_id).get_data()
+
+	del sensitivity
+
+	if mode in ["division", "divide", "div"]:
+		data_normalized = data.get_datafield(data_id).get_data() / refquantity
+	elif mode in ["subtraction", "subtract", "sub"]:
+		if refquantity.dtype == np.dtype('uint'):
+			refquantity = refquantity.astype('int')  # To avoid unsigned integer overflow.
+		data_normalized = data.get_datafield(data_id).get_data() - refquantity
+	else:
+		raise ValueError("Unrecognized mode for normalize_sensitivity.")
+
+	del refquantity
+
+	data_normalized[~ np.isfinite(data_normalized)] = 0  # set inf, and NaN to 0
+	data.add_datafield(data_normalized, label=newlabel+'_reference_'+mode, plotlabel=new_plotlabel+'_reference_'+mode)
+	return data
