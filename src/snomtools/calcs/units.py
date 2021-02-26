4a5f31fb40c0df176ac13dba1675e865c98f4aae
Michael Hartelt <hartelt@physik.uni-kl.de>
2021-02-26 16:40:05 +0100

    calcs.units: Format indents.

--- src/snomtools/calcs/units.py
+++ src/snomtools/calcs/units.py
@@ -33,190 +33,190 @@ ureg.add_context(c)
 
 
 def same_dimension(*args):
-	"""
-	Checks if all of the given arguments are of the same (physical) dimension.
+    """
+    Checks if all of the given arguments are of the same (physical) dimension.
 
-	:param args:
+    :param args:
 
-	:return: Boolean.
-	"""
-	uregargs = as_ureg_quantities(args)
-	# with as_ureg_quantities(args) as a:
-	arg0 = next(uregargs)
-	for arg in uregargs:
-		if arg0.dimensionality != arg.dimensionality:
-			return False
-	return True
+    :return: Boolean.
+    """
+    uregargs = as_ureg_quantities(args)
+    # with as_ureg_quantities(args) as a:
+    arg0 = next(uregargs)
+    for arg in uregargs:
+        if arg0.dimensionality != arg.dimensionality:
+            return False
+    return True
 
 
 def same_unit(*args):
-	"""
-	Checks if all of the given arguments are of the same unit.
+    """
+    Checks if all of the given arguments are of the same unit.
 
-	:param args:
+    :param args:
 
-	:return: Boolean.
-	"""
-	uregargs = as_ureg_quantities(args)
-	# with as_ureg_quantities(args) as a:
-	arg0 = next(uregargs)
-	for arg in uregargs:
-		if arg0.units != arg.units:
-			return False
-	return True
+    :return: Boolean.
+    """
+    uregargs = as_ureg_quantities(args)
+    # with as_ureg_quantities(args) as a:
+    arg0 = next(uregargs)
+    for arg in uregargs:
+        if arg0.units != arg.units:
+            return False
+    return True
 
 
 def is_valid_unit(tocheck):
-	"""
-	Checks if a string can be interpreted as a unit.
+    """
+    Checks if a string can be interpreted as a unit.
 
-	:param tocheck: String, The input to check.
+    :param tocheck: String, The input to check.
 
-	:return: Boolean if the input can be interpreted as a unit.
-	"""
-	try:
-		ureg(tocheck)
-		return True
-	except pint.UndefinedUnitError:
-		return False
+    :return: Boolean if the input can be interpreted as a unit.
+    """
+    try:
+        ureg(tocheck)
+        return True
+    except pint.UndefinedUnitError:
+        return False
 
 
 def unit_from_str(tocheck):
-	"""
-	Checks if a string can be interpreted as a unit and returns the corresponding unit.
+    """
+    Checks if a string can be interpreted as a unit and returns the corresponding unit.
 
-	:param tocheck: String, The input to check.
+    :param tocheck: String, The input to check.
 
-	:return: Quantity with magnitude 1 and the corresponding unit, or None if not castable.
-	"""
-	try:
-		unit = ureg(tocheck)
-		return unit
-	except pint.UndefinedUnitError:
-		return None
+    :return: Quantity with magnitude 1 and the corresponding unit, or None if not castable.
+    """
+    try:
+        unit = ureg(tocheck)
+        return unit
+    except pint.UndefinedUnitError:
+        return None
 
 
 def normalize_unitstr(unitstr):
-	"""
-	Returns the normalized unit string, meaning the string conversion of the casted unit.
+    """
+    Returns the normalized unit string, meaning the string conversion of the casted unit.
 
-	:param unitstr: string: to convert
+    :param unitstr: string: to convert
 
-	:return: string: converted
-	"""
-	return str(ureg(str(unitstr)).units)
+    :return: string: converted
+    """
+    return str(ureg(str(unitstr)).units)
 
 
 def is_quantity(tocheck):
-	"""
-	Tries if the given object is a pint quantity.
+    """
+    Tries if the given object is a pint quantity.
 
-	:param tocheck: The object to check.
+    :param tocheck: The object to check.
 
-	:return: Bool.
-	"""
-	return isinstance(tocheck, pint.quantity._Quantity)
+    :return: Bool.
+    """
+    return isinstance(tocheck, pint.quantity._Quantity)
 
 
 def to_ureg(input_, unit=None, convert_quantities=True):
-	"""
-	This method is an import function to import alien quantities (of different unit registries) or numeric formats into
-		the ureg.
+    """
+    This method is an import function to import alien quantities (of different unit registries) or numeric formats into
+        the ureg.
 
-	:param input_: The input quantity or numeric format (e.g. float, int, numpy array) or string (e.g. "5 nm")
+    :param input_: The input quantity or numeric format (e.g. float, int, numpy array) or string (e.g. "5 nm")
 
-	:param unit: Given as a valid unit string. If a numeric format is used, this specifies the unit of it. If a quantity
-		is used, the output quantity will be converted to it.
+    :param unit: Given as a valid unit string. If a numeric format is used, this specifies the unit of it. If a quantity
+        is used, the output quantity will be converted to it.
 
-	:param convert_quantities: Boolean: Specifies what should be done if input_ is a quantity.
-		True: convert the quantity to the unit if specified by unit parameter. (The default)
-		False: only assure the dimension is correct and import to ureg, but leave unit of input_ as is is.
+    :param convert_quantities: Boolean: Specifies what should be done if input_ is a quantity.
+        True: convert the quantity to the unit if specified by unit parameter. (The default)
+        False: only assure the dimension is correct and import to ureg, but leave unit of input_ as is is.
 
-	:return: The imported quantity.
-	"""
-	# Python2/3 compatibility: If unit is a byte string, decode it for usage in pint.Quantity.
-	if isinstance(unit, bytes):
-		unit = unit.decode()
+    :return: The imported quantity.
+    """
+    # Python2/3 compatibility: If unit is a byte string, decode it for usage in pint.Quantity.
+    if isinstance(unit, bytes):
+        unit = unit.decode()
 
-	# Check if input is quantity:
-	if is_quantity(input_):
-		# If output unit is specified, make sure it has a compatible dimension. Else a DimensionalityError will be
-		# raised:
-		if unit and not same_dimension(input_, to_ureg(1, unit)):
-			raise pint.DimensionalityError(input_.units, to_ureg(1, unit).units,
-										   extra_msg="Incompatible units given to to_ureg!")
-		# Check if input is already of our ureg. If so, nothing to do other then convert if unit is specified:
-		if input_._REGISTRY is ureg:
-			if unit and convert_quantities and input_.units != to_ureg(1, unit).units:
-				return input_.to(unit)
-			else:
-				return input_
-		else:  # Use inputs magnitude, but our corresponding ureg unit.
-			if unit and convert_quantities:
-				return Quantity(input_.magnitude, str(input_.units)).to(unit)
-			else:
-				return Quantity(input_.magnitude, str(input_.units))
-	elif (isinstance(input_, str) or isinstance(input_, str)):
-		if unit:
-			return ureg(input_).to(unit)
-		else:
-			return ureg(input_)
-	else:  # we are dealing with numerical data
-		return Quantity(input_, unit)
+    # Check if input is quantity:
+    if is_quantity(input_):
+        # If output unit is specified, make sure it has a compatible dimension. Else a DimensionalityError will be
+        # raised:
+        if unit and not same_dimension(input_, to_ureg(1, unit)):
+            raise pint.DimensionalityError(input_.units, to_ureg(1, unit).units,
+                                           extra_msg="Incompatible units given to to_ureg!")
+        # Check if input is already of our ureg. If so, nothing to do other then convert if unit is specified:
+        if input_._REGISTRY is ureg:
+            if unit and convert_quantities and input_.units != to_ureg(1, unit).units:
+                return input_.to(unit)
+            else:
+                return input_
+        else:  # Use inputs magnitude, but our corresponding ureg unit.
+            if unit and convert_quantities:
+                return Quantity(input_.magnitude, str(input_.units)).to(unit)
+            else:
+                return Quantity(input_.magnitude, str(input_.units))
+    elif (isinstance(input_, str) or isinstance(input_, str)):
+        if unit:
+            return ureg(input_).to(unit)
+        else:
+            return ureg(input_)
+    else:  # we are dealing with numerical data
+        return Quantity(input_, unit)
 
 
 def as_ureg_quantities(stream, unit=None):
-	for e in stream:
-		yield to_ureg(e, unit)
+    for e in stream:
+        yield to_ureg(e, unit)
 
 
 def magnitudes(stream):
-	for e in stream:
-		yield e.magnitude
+    for e in stream:
+        yield e.magnitude
 
 
 def units(stream):
-	for e in stream:
-		yield e.units
+    for e in stream:
+        yield e.units
 
 
 def meshgrid(*args):
-	"""
-	Does the same as numpy.meshgrid, but preserves units.
+    """
+    Does the same as numpy.meshgrid, but preserves units.
 
-	:return:
-	"""
-	unitbuffer = []
-	for x in args:
-		unitbuffer.append(str(x.units))
-	gridtup = numpy.meshgrid(*args)
-	outlist = []
-	for i in range(len(gridtup)):
-		outlist.append(to_ureg(gridtup[i], unitbuffer[i]))
-	return tuple(outlist)
+    :return:
+    """
+    unitbuffer = []
+    for x in args:
+        unitbuffer.append(str(x.units))
+    gridtup = numpy.meshgrid(*args)
+    outlist = []
+    for i in range(len(gridtup)):
+        outlist.append(to_ureg(gridtup[i], unitbuffer[i]))
+    return tuple(outlist)
 
 
 def latex_si(input_):
-	"""
-	Returns a LaTeX \si command for a correct LaTeX representation of the unit.
+    """
+    Returns a LaTeX \si command for a correct LaTeX representation of the unit.
 
-	:param input_: Must be castable as a quantity.
+    :param input_: Must be castable as a quantity.
 
-	:return: str: The command string, e.g. '\\si[]{\\femto\\second}'.
-	"""
-	input_ = to_ureg(input_)
-	# noinspection PyStringFormat
-	return "{:Lx}".format(input_.units).replace("electron_volt", "electronvolt")
+    :return: str: The command string, e.g. '\\si[]{\\femto\\second}'.
+    """
+    input_ = to_ureg(input_)
+    # noinspection PyStringFormat
+    return "{:Lx}".format(input_.units).replace("electron_volt", "electronvolt")
 
 
 def latex_SI(input_):
-	"""
-	Returns a LaTeX \SI command for a correct LaTeX representation of the quantity.
+    """
+    Returns a LaTeX \SI command for a correct LaTeX representation of the quantity.
 
-	:param input_: Must be castable as a quantity.
+    :param input_: Must be castable as a quantity.
 
-	:return: str: The command string, e.g. '\\SI[]{1}{\\femto\\second}'.
-	"""
-	input_ = to_ureg(input_)
-	# noinspection PyStringFormat
-	return "{:Lx}".format(input_).replace("electron_volt", "electronvolt")
+    :return: str: The command string, e.g. '\\SI[]{1}{\\femto\\second}'.
+    """
+    input_ = to_ureg(input_)
+    # noinspection PyStringFormat
+    return "{:Lx}".format(input_).replace("electron_volt", "electronvolt")
