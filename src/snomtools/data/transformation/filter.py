e0cf4c73c559fc2a5d7e367349957650a3096792
Michael Hartelt <hartelt@physik.uni-kl.de>
2021-03-30 16:52:51 +0200

    data.transformation.filter.py added

---
+++ src/snomtools/data/transformation/filter.py
@@ -0,0 +1,9 @@
+"""
+Filters working on DataSets using scipy.ndimage filters.
+"""
+
+import numpy as np
+import scipy.ndimage
+import snomtools.data.datasets as ds
+import snomtools.calcs.units as u
+
